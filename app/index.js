69640ed69c7c084721dbcb26479679af37b5d7f0
ghoullier <gregory.houllier@gmail.com>
2014-10-17 08:51:47 +0200

    Add initial behavior

---
+++ app/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path');
+var chalk = require('chalk');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+var AngularDevstackGenerator = yeoman.generators.Base.extend({
+  /* Initialization, evaluate appname */
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.argument('appname', { type: String, required: false });
+    this.appname = this.appname || path.basename(process.cwd());
+  },
+
+  info: function () {
+    this.log(yosay(
+      chalk.red('Welcome!') + '\n' +
+      chalk.yellow('You\'re using the fantastic generator for scaffolding an application with Angular and Gulp!')
+    ));
+  },
+
+  /* Install dependencies */
+  install: function () {
+    this.installDependencies();
+  }
+});
+
+module.exports = AngularDevstackGenerator;
