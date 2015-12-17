4cebb3a4d7993879f18f72c67e1a443379024f07
ghoullier <gregory.houllier@gmail.com>
2015-12-17 09:17:25 +0100

    fix(component) Fix genereated controller alias

--- generators/component/templates/_router.js
+++ generators/component/templates/_router.js
@@ -6,7 +6,7 @@ export default ($stateProvider) => {
     .state('<%= component %>', {
       url: '/<%= component %>',
       templateUrl: 'components/<%= component %>/partials/<%= component %>.html',
-      controller: '<%= controller %> as vm'
+      controller: '<%= controller %> as <%= component %>'
     })
-  
+
 }
