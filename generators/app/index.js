66a85e0a021d7c84841518f4d02a0c24b715d19e
ghoullier <gregory.houllier@gmail.com>
2016-02-03 09:15:22 +0100

    feat(yo) Support yo 0.22

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,7 +5,7 @@ var yeoman = require('yeoman-generator');
 var prompting = require('./src/prompting');
 var writing = require('./src/writing');
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = yeoman.Base.extend({
   initializing: function () {
     this.pkg = require('../../package.json');
 
