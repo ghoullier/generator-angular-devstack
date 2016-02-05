bb410e2acb3de76323de86530641439c1ad02729
ghoullier <gregory.houllier@gmail.com>
2016-02-05 11:38:33 +0100

    feat(styles) Support css vendor compilation

--- generators/app/templates/tasks/styles.js
+++ generators/app/templates/tasks/styles.js
@@ -6,30 +6,47 @@ import plumber from 'gulp-plumber'
 import sourcemaps from 'gulp-sourcemaps'
 import concat from 'gulp-concat-css'
 import browserSync from 'browser-sync'
+import files from 'main-bower-files'
 
-import args from './utils/cli-args'
-import paths from './utils/paths'
+import { optimize } from './utils/cli-args'
 import { onGenericError } from './utils/handlers'
+import paths from './utils/paths'
 
-export default () => {
-  const optimize = args.optimize
-  return gulp.src(paths.sources.styles)
+export const app = () => {
+  return transform(gulp.src(paths.sources.styles), 'main.css', [
+    sass({
+      onError: onGenericError,
+      sourceComments: optimize ? false : 'map',
+      outputStyle: optimize ? 'compressed' : 'expanded'
+    }),
+    optimize ? autoprefixer('last 2 versions', '> 1%') : util.noop()
+  ])
+}
+
+export const vendor = () => {
+  const source = files({
+    filter: /^(.)*(\.css)$/
+  })
+  return transform(gulp.src(source), 'vendor.css')
+}
+
+const transform = (stream, filename, tasks = []) => {
+  let transformed = stream
     // Catch errors
     .pipe(plumber({
       errorHandler: onGenericError
     }))
     // Prepare sourcemaps generation
     .pipe(optimize ? util.noop() : sourcemaps.init())
-    // The onerror handler prevents Gulp from crashing when you make a mistake in your SASS
-    .pipe(sass({
-      onError: onGenericError,
-      sourceComments: optimize ? false : 'map',
-      outputStyle: optimize ? 'compressed' : 'expanded'
-    }))
-    // Optionally add autoprefixer
-    .pipe(optimize ? autoprefixer('last 2 versions', '> 1%') : util.noop())
+
+  // Execute extra transform tasks
+  tasks.forEach((task) => {
+    transformed = transformed.pipe(task)
+  })
+
+  return transformed
     // Concat files
-    .pipe(concat('main.css'))
+    .pipe(concat(filename))
     // Generate sourcemaps
     .pipe(optimize ? util.noop() : sourcemaps.write())
     // These last two should look familiar now :)
